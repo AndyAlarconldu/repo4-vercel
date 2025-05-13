b1b872abfa08ef77ce3eccdf332ca78f0b64afba
AndyAlarconldu <andyjavier301067@gmail.com>
2025-05-13 11:54:31 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {
+  // Aquí iría el código para enviar correo (puede ser con Nodemailer o Mailgun)
+  res.status(200).json({ mensaje: "Correo enviado correctamente" });
+}
